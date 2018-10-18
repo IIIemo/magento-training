9ac65846069f85980d8e1345fa7ebf8415da2790
Eduard Paliy <epaliy@oggettoweb.com>
2018-10-19 00:07:43 +0300

    Add promo module

---
+++ app/code/Oggetto/Promo/Controller/Index/Index.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Oggetto\Promo\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    private $pageFactory;
+
+    public function __construct(PageFactory $pageFactory, Context $context)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
